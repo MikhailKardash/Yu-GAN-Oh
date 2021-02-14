926a1e5160a384b4eab855af6ad69e7f57b8e4a1
MikhailKardash <48943383+MikhailKardash@users.noreply.github.com>
2021-02-13 20:17:45 -0800

    Update yuGANoh_with_fc_and_disc.py
    Annotated each class and most of the functions. Next steps are to make it pep8 compliant.

--- yuGANoh_with_fc_and_disc.py
+++ yuGANoh_with_fc_and_disc.py
@@ -4,7 +4,14 @@ import torch
 
 
 class Generator(nn.Module):
+    '''
+    Generator Network, DCGAN-like architecture with LeakyRELU and custom kernel shapes.
+    '''
     def __init__(self,noise_channels,gan_features):
+        '''
+        :param noise_channels: input noise dimension
+        :param gan_features: number of channel features. scales network width.
+        '''
         super(Generator, self).__init__()
         self.main = nn.Sequential(
             # input is Z, going into a convolution
@@ -37,7 +44,19 @@ class Generator(nn.Module):
     
     
 class Discriminator(nn.Module):
+    '''
+    Discriminator Network, based of DCGAN, but with leakyRELU and a few differences.
+    LeakyReLU is used, along with minibatch discrimination in the form of
+    similarity_features.
+    '''
     def __init__(self, disc_features,num_features,similarity_features,batch_size):
+        '''
+        :param disc_features: number of gan features, scales network width
+        :param num_features: number of features to be given to FC layer.
+        :param similarity_features: number of similarity features
+               for minibatch discrimination
+        :param batch_size: redundant, will be removed in a later version.
+        '''
         super(Discriminator,self).__init__()
         self.num_features = num_features
         self.T = torch.nn.Parameter(torch.randn(similarity_features,similarity_features,num_features*2),requires_grad=True)
@@ -60,11 +79,15 @@ class Discriminator(nn.Module):
             # state size. (disc_features*8) x 4 x 4
             nn.Conv2d(disc_features * 8, num_features*2, 4, 1, 0, bias=False)
         )
-        #self.fc1 = nn.Linear(num_features*2,num_features,bias=False)
         self.fc1 = nn.Linear(num_features*2,num_features,bias=False)
         self.fc2 = nn.Linear(num_features+similarity_features,1,bias=False)
 
     def forward(self, input):
+        '''
+        Discriminator forward, minibatch discrimination implemented.
+        :param input: Input data tensor
+        :return: label for CE loss and batch features for mean loss.
+        '''
         features = self.main(input)
         features = features.squeeze()
         if len(features.shape) == 1:
@@ -79,6 +102,12 @@ class Discriminator(nn.Module):
         return features, label
 
 def init_weights(m):
+    '''
+    Weight initializers.
+    Pass in a network layer to be initialized.
+    :param m: reference to a layer object
+    :return: None
+    '''
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         nn.init.normal_(m.weight.data, 0,0.02)
